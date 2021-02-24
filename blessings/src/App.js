391cddaa6ff2db611fa241f264014fc88e7d3228
jcanete <joachimcanete@gmail.com>
2021-02-24 13:55:46 -0600

    added components dir

--- blessings/src/App.js
+++ blessings/src/App.js
@@ -1,9 +1,11 @@
 import './App.css';
+import Navigation from './Components/Navigation'
 
 function App() {
   return (
     <div className="App">
       <h1>The Bite-Sized Book Of Itty Bitty Blessings</h1>
+      <Navigation/>
     </div>
   );
 }
