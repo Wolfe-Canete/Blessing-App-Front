1329f0a1e70ec07f516255b6a3e91cf1d4f15264
jcanete <joachimcanete@gmail.com>
2021-02-24 13:51:56 -0600

    initial front end react scaffold

---
+++ blessings/src/App.js
@@ -0,0 +1,11 @@
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <h1>The Bite-Sized Book Of Itty Bitty Blessings</h1>
+    </div>
+  );
+}
+
+export default App;
