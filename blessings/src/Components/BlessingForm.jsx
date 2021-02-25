d2eb9f681d65636d7570a480b16a9d427a1ed194
jcanete <joachimcanete@gmail.com>
2021-02-25 17:18:04 -0600

    basic form skeleton for blessing submission

---
+++ blessings/src/Components/BlessingForm.jsx
@@ -0,0 +1,32 @@
+import { useState } from 'react'
+
+export default function BlessingForm() {
+    const initialState = {
+        author:'',
+        title:'',
+        cont:''
+    }
+
+    const [formState, setFormState] = useState(initialState)
+
+    const handleSubmit = (event) => {
+        event.preventDefault()
+        console.log(formState)
+        setFormState({ ...formState, [event.target.id]: event.target.value})
+    }
+
+    const handleChange = (event) => {
+        setFormState({ ...formState, [event.target.id]: event.target.value})
+    }
+
+    return (
+        <form onSubmit={handleSubmit}>
+            <label>
+                Count Your Blessings:
+                <input type='text' name='name'/>
+            </label>
+            <input type='submit' value='Submit'/>
+        </form>
+
+    )
+}
