59f899b193e29069622a06b6735de42d4cb2fda7
jcanete <joachimcanete@gmail.com>
2021-02-27 10:40:32 -0600

    devloping form funcitonality

--- blessings/src/Components/BlessingForm.jsx
+++ blessings/src/Components/BlessingForm.jsx
@@ -1,4 +1,6 @@
 import { useState } from "react";
+import './styles/FormStyle.css'
+import Button from 'react-bootstrap/Button';
 import Form from "react-bootstrap/Form";
 
 export default function BlessingForm() {
@@ -8,32 +10,101 @@ export default function BlessingForm() {
     cont: "",
   };
 
-  const [formState, setFormState] = useState(initialState);
+  const [blessing, setBlessing] = useState(initialState);
 
   const handleSubmit = (event) => {
     event.preventDefault();
-    console.log(formState);
-    setFormState({ ...formState, [event.target.id]: event.target.value });
+    console.log(blessing);
+    setBlessing({ ...blessing, [event.target.id]: event.target.value });
   };
 
   const handleChange = (event) => {
-    setFormState({ ...formState, [event.target.id]: event.target.value });
+    setBlessing({ ...blessing, [event.target.id]: event.target.value });
   };
 
+
+  /*
+  axios.post('https://nameless-citadel-52825.herokuapp.com/blessings/', {
+    author,
+    title,
+    blessing
+  }).then((resp) => {
+    addBlessing(resp.data)
+  })
+
+  function addPictures (gifData) {
+  // Adds all of the gifs to the dom
+  gifs.innerHTML = ''
+  gifData.forEach(gif => {
+    if (!gif.url) return
+
+    const imageNode = document.createElement('img')
+    imageNode.setAttribute('src', gif.url)
+    imageNode.classList.add('gif')
+
+    imageNode.addEventListener('click', () => { editModal(gif) })
+
+    gifs.appendChild(imageNode)
+  })
+  }
+
+//*----= OR =----*\\
+
+   const postBlessing = async () => {
+      const url =
+         process.env.NODE_ENV === 'production'
+            ? 'https://nameless-citadel-52825.herokuapp.com/blessings/'
+            : 'http://localhost:8000/blessings';
+      const headers = { 'Content-Type': 'application/json' };
+
+      try {
+         const response = await axios.post(url, blessing, {
+            headers: headers
+         });
+         return response.data._id;
+      } catch (error) {
+         console.error(error);
+      }
+   };
+  */
+
   return (
-    <Form>
-      <Form.Group controlId="exampleForm.ControlInput1">
+    <Form className='new-blessing' onSubmit={handleSubmit}>
+
+      <Form.Group className='input-author' controlId="exampleForm.ControlInput1">
         <Form.Label>Author</Form.Label>
-        <Form.Control type="input" placeholder="Author Name" />
+        <Form.Control
+          type="input"
+          value={blessing.author}
+          placeholder="Author Name"
+          onChange={handleChange}
+          />
       </Form.Group>
-      <Form.Group controlId="exampleForm.ControlTextarea1">
-        <Form.Label>Post</Form.Label>
+
+      <Form.Group className='input-title' controlId="exampleForm.ControlInput2">
+        <Form.Label>Title</Form.Label>
+        <Form.Control
+          type="input"
+          value={blessing.title}
+          placeholder="Blessing's Title"
+          onChange={handleChange}
+          />
+      </Form.Group>
+
+      <Form.Group className='input-blessing' controlId="exampleForm.ControlTextarea3">
+        <Form.Label>Blessing</Form.Label>
         <Form.Control
           as="textarea"
+          value={blessing.content}
           rows={3}
           placeholder="Count your blessings"
+          onChange={handleChange}
         />
       </Form.Group>
+      
+      <Button className='submit-blessing' type='submit' variant='success'>
+          SUBMIT
+      </Button>
     </Form>
   );
 }
