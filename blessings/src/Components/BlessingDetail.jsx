b50c69da112111702cf66d5e130d5004085c349f
jcanete <joachimcanete@gmail.com>
2021-02-28 19:18:49 -0600

    rendered individual blessings

--- blessings/src/Components/BlessingDetail.jsx
+++ blessings/src/Components/BlessingDetail.jsx
@@ -2,38 +2,36 @@ import Card from 'react-bootstrap/Card'
 import { useState, useEffect } from 'react'
 
 export default function BlessingDetail({ match }) {
-const [blessing, setBlessing] = useState(null);
+    const [blessedDetails, setblessedDetails] = useState(null);
 
-    const fetchBlessings = async () => {
-        await fetch(`https://nameless-citadel-52825.herokuapp.com/blessings/${match.params.id}`)
+    const fetchBlessingedDetails = async () => {
+        await fetch(`http://127.0.0.1:8000/blessings/${match.params.id}`)
         .then(res => res.json())
         .then(res => {
-            setBlessing(res)
             console.log(res)
+            setblessedDetails(res)
         })
     }
     useEffect(() => {
-        fetchBlessings()
-    }, []);
+        fetchBlessingedDetails()
+    },);
 
     return(
-        blessing && (
-    <div>
-        <Card style={{ width: "20rem" }}>
-        <Card.Body>
-            <Card.Title>{blessing.title}</Card.Title>
-            <Card.Subtitle className="mb-2 text-muted">
-            {blessing.author}
-            </Card.Subtitle>
-            <Card.Text>
-            {blessing.content}
-            </Card.Text>
-            <Card.Link href="#">Card Link</Card.Link>
-        </Card.Body>
-        </Card>
-    </div>
+        blessedDetails && (
+            <div>
+                <Card style={{ width: "20rem" }}>
+                    <Card.Body>
+                        <Card.Title>{blessedDetails.title}</Card.Title>
+                    <Card.Subtitle className="mb-2 text-muted">
+                        {blessedDetails.author}
+                    </Card.Subtitle>
+                    <Card.Text>
+                        {blessedDetails.content}
+                    </Card.Text>
+                    </Card.Body>
+                </Card>
+            </div>
         )
-)
-
+    )
 }
 
